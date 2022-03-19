d2a683498235dbfd0dcdc10b3e3a37aeef3c0d32
Guidenevada <67476134+Guidenevada@users.noreply.github.com>
2022-03-20 00:32:15 +0100

    Update networker.py

--- networker.py
+++ networker.py
@@ -12,9 +12,9 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
 def connection(ip):
-    for x in range(10):
+    for x in range(5):
         try:
-            sock.connect((str(ip), 4443))
+            sock.connect((ip, 4448))
             break
 
         except:
@@ -26,16 +26,32 @@ def send(message):
         sock.send(message.encode())
     except Exception as e :
         os.system("echo '" +e+"' > logs/log"+now.day+ "-" +now.hour+":"+now.minute+","+now.second+".txt")
-        print("failed to send message\n\nlogs have been saved in log directory")
+        print("failed to send message\n\nlogs have been saved in log directory\n\n\n"+ e)
         
 def main():
     print("hello to my network app")
-    ip = input("what is the server ip?")
-    connection(ip)
-    a = 1
+    #ip = input("what is the server ip?")
+    ip = '127.0.0.1'
+    #connection(ip)
+    sock.connect((ip, 4448))
+    #conn, addr = sock.accept()
+    #print("connected to " + str(addr))
+    sock.send("new connection".encode())
+    a = int(1)
     while (a == 1):
-        message = input("")
-        send(message)
-
+        
+        while sock.recv(1024):
+            print("Recived: "+ sock.recv(1024).decode())
+        
+        message = input(">")
+        sock.send(message.encode())
+        if message == "exit":
+            a = 0
+            
+            break
+    if a == 0:
+        sock.close()
+        print("connection closed")
+        
 if __name__ == '__main__':
     main()
